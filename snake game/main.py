63db628aa98c2bd648abbb838d819cfbc8655766
evilzeda <thariqibrahim1998@gmail.com>
2025-01-07 22:37:28 +0700

    add new project -> snake game

---
+++ snake game/main.py
@@ -0,0 +1,27 @@
+import pygame
+import time
+import random
+
+# print(f"pygame version is: {pygame.__version__}")
+
+#init pygame
+pygame.init()
+
+#define color
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+orange = (255, 165, 0)
+
+width, height = 600, 400
+
+game_display = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Orochimaru Snake Game")
+
+clock = pygame.time.clock()
+
+snake_size = 10
+snake_speed = 15
+
+message_font = pygame.font.SysFont('ubuntu', 30)
+score_font = pygame.font.SysFont('ubuntu', 25)
